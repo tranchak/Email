afec9f81039bb07af6e7adcc5b6eba6a6ce90dea
oleg <olegpustovalov220@gmail.com>
2022-01-23 16:32:18 +0300

    https://github.com/OlegEgoism/email.git 123 1231

--- accaunt/views.py
+++ accaunt/views.py
@@ -24,6 +24,7 @@ class ApiUserRegistartionView(APIView):
             print(serializer.validated_data)
             return Response(serializer.data)
 
+
     def get(self, request):
         user = User.objects.all()
         serializer = UserRegisterationsSerializer(user, many=True)
